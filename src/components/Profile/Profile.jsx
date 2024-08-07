49a53b00475d283a9b2e0a1a8e1131f302188d30
BigBlackJoke <155197294+BigBlackJoke@users.noreply.github.com>
2024-08-07 15:22:49 +0300

    start

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,34 @@
+import s from './Profile.module.css';
+
+const Profile = (name, tag, location, image, stats) => {
+    return (
+        <div>
+          <div>
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+              alt="User avatar"
+            />
+            <p>{name}</p>
+            <p>{tag}</p>
+            <p>{location}</p>
+          </div>
+
+          <ul>
+            <li>
+              <span>Followers</span>
+              <span>1000</span>
+            </li>
+            <li>
+              <span>Views</span>
+              <span>2000</span>
+            </li>
+            <li>
+              <span>Likes</span>
+              <span>3000</span>
+            </li>
+          </ul>
+        </div>
+)
+}
+
+export default;
